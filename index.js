7d9f1a19ae616fe0ab4c2b960d39a2bcc0ee9b3d
Taylor Shen <shen.taylor@yahoo.com>
2022-06-17 21:04:45 -0700

    adding inquirer prompts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const generateHtml = require('./util/generateHtml')
+const generate = require('./util/generateHtml')
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
@@ -17,17 +17,79 @@ const buildTeam = () => {
     ]).then(ans => {
         switch (ans.options) {
             case 'Add Engineer':
+                addEngineer();
                 break;
             case 'Add Intern':
+                addIntern();
                 break;
             case 'Finish Building Team':
+                fs.writeFileSync("./util/index.html", generate(teamMembers));
+                console.log("Team generated.")
                 break;
             default:
                 console.log('Choose an option.');
                 break;
         }
     })
-}
+};
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name of engineer:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter id of engineer:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email of engineer:'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter github of engineer:'
+        },
+    ]).then(res => {
+        const engineer = new Engineer(res.name, res.id, res.email, res.github);
+        teamMembers.push(engineer);
+        buildTeam();
+    })
+};
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name of intern:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter id of intern:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email of intern:'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter school of intern:'
+        },
+    ]).then(res => {
+        const intern = new Intern(res.name, res.id, res.email, res.school);
+        teamMembers.push(intern);
+        buildTeam();
+    })
+};
 
 const createManager = () => {
     inquirer.prompt([
